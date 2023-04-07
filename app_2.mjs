6ee64df6d02bd244d41ae33e03c942ca929383be
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-04-08 00:39:50 +0400

    update s3 and account

--- app_2.mjs
+++ app_2.mjs
@@ -14,18 +14,14 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { v4 as uuid } from 'uuid';
 
 const s3 = new S3Client({
-  region: 'us-east-1',
+  region: process.env.REGION,
   credentials: {
     accessKeyId: process.env.ACCESS_KEY_ID,
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 });
 
-// const BUCKET = process.env.BUCKET;
-
-const BUCKET = 'ahmedalimsolimanpics';
-
-// ahmedamsoliman;
+const BUCKET = process.env.BUCKET;
 
 export const uploadToS3 = async ({ file, userId }) => {
   const key = `${userId}/${uuid()}`;
